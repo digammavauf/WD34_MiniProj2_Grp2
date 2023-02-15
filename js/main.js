d226be7c1d4d027df4615c177b1c2c72d810c592
digammavauf <johnphiliptena@digammavauf.com>
2023-02-15 20:17:13 +0800

    Update: Rename Group

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /* BEGIN CREATE AND GENERATE NAVBAR */
 import { navBar } from "./nav.js";
-navBar.brand = "TCL";
+navBar.brand = "TLC";
 navBar.icon = "img/Tena.jpg";
 navBar.add("fa-user-doctor", "Services", "services.html", "Our services");
 navBar.add("fa-circle-info", "About", "about.html", "About us");
